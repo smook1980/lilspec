a93da3ba3726af046e988163912ce22de0a8cba7
Shane Mook <shane.mook@gmail.com>
2017-05-06 15:03:39 -0500

    Added additional test blocks (describe, context, it), BeforeEach and gomock controller support.

--- spec.go
+++ spec.go
@@ -1,22 +1,28 @@
 package lilspec
 
 import (
+	"sync"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/onsi/gomega"
 )
 
 type TCtx interface {
 	Errorf(format string, args ...interface{})
 	Run(string, func(*testing.T)) bool
+	Fatalf(format string, args ...interface{})
 }
 
 // S provides BDD Spec features
 type S struct {
+	bfns []func(s *S)
 	TCtx
+	once     sync.Once
+	mockCtrl *gomock.Controller
 }
 
-// S returns a base spec given t
+// T returns a base spec given t
 func T(t TCtx) *S {
 	return &S{TCtx: t}
 }
@@ -27,16 +33,41 @@ func (s *S) Expect(it interface{}, e ...interface{}) gomega.GomegaAssertion {
 	return gomega.Expect(it, e...)
 }
 
-// Describe begins a sub test with the given description.
-func (s *S) Describe(desc string, descb func(*S)) {
+func (s *S) mock() *gomock.Controller {
+	s.once.Do(func() {
+		s.mockCtrl = gomock.NewController(s.TCtx)
+	})
+
+	return s.mockCtrl
+}
+
+func (s *S) specBlock(desc string, descb func(*S)) {
+	for _, bfn := range s.bfns {
+		bfn(s)
+	}
 	s.TCtx.Run(desc, func(t *testing.T) {
 		descb(T(t))
 	})
+	if s.mockCtrl != nil {
+		s.mockCtrl.Finish()
+	}
 }
 
 // Describe begins a sub test with the given description.
-func (s *S) When(desc string, descb func(*S)) {
-	s.TCtx.Run(desc, func(t *testing.T) {
-		descb(T(t))
-	})
+func (s *S) Describe(desc string, block func(*S)) {
+	s.specBlock(desc, block)
+}
+
+// Context begins a sub test with the given description.
+func (s *S) Context(desc string, block func(*S)) {
+	s.specBlock(desc, block)
+}
+
+// It begins a sub test with the given description.
+func (s *S) It(desc string, block func(*S)) {
+	s.specBlock(desc, block)
+}
+
+func (s *S) BeforeEach(bfn func(s *S)) {
+	s.bfns = append(s.bfns, bfn)
 }
--- spec_test.go
+++ spec_test.go
@@ -21,11 +21,37 @@ func TestSpec(t *testing.T) {
 		})
 	})
 
-	t.Run("s.When", func(it *testing.T) {
-		T(it).When("when blocks", func(s *S) {
+	t.Run("spec blocks", func(t *testing.T) {
+		T(t).Context("when a context block is given", func(s *S) {
 			s.Expect(true).To(BeTrue())
-			s.When("nested", func(s *S) {
+			s.It("has a nested scope", func(s *S) {
 				s.Expect(true).To(BeTrue())
+			})
+		})
+	})
+
+	t.Run("BeforeEach", func(t *testing.T) {
+		T(t).Describe("BeforeEach", func(s *S) {
+			x := 1
+			s.BeforeEach(func(s *S) {
+				x = x + 1
+			})
+
+			s.It("runs before each block", func(s *S) {
+				s.Expect(x).To(Equal(2))
+				y := x
+
+				s.BeforeEach(func(s *S) {
+					y = y + 1
+				})
+
+				s.It("test block", func(s *S) {
+					s.Expect(y).To(Equal(3))
+				})
+			})
+
+			s.It("runs before each block", func(s *S) {
+				s.Expect(x).To(Equal(3))
 			})
 		})
 	})
